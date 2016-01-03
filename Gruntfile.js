ad94eb9f86199ee813d53ead6523ddcfbafafe9f
Stephan van Rooij <stephan.vanrooij@fontys.nl>
2016-01-03 23:50:30 +0100

    Linted files
    All the files are now linted. This means they are all validated and
    have correct indentation.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,15 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
     uglify: {
       options: {
         compress: {
           drop_console: true
-        }
+        },
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - https://smartersoft.github.io/CookiebarGone/ */',
       },
-      my_target: {
+      safari: {
         files: {
           'src/CookiebarGone.safariextension/cookiebar-gone.min.js': ['src/scripts/main_data.js', 'src/scripts/main_logic.js']
         }
@@ -16,49 +17,62 @@ module.exports = function(grunt) {
     },
 
     fileExists: {
-      safariextension: ['dist/CookiebarGone.safariextz','dist/CookiebarGone-updates.plist']
+      safariextension: ['dist/CookiebarGone.safariextz', 'dist/CookiebarGone-updates.plist']
     },
 
-		copy: {
-  		main: {
-				files: [
-					{expand: true,cwd: 'src/web/',src: '**',dest: 'dist/'}
-				]
+    copy: {
+      main: {
+        files: [{
+          expand: true,
+          cwd: 'src/web/',
+          src: '**',
+          dest: 'dist/'
+        }]
 
-  		},
-		},
+      },
+    },
 
-		'gh-pages': {
-			options: {
-				base: 'dist'
-			},
-			src: '**/*'
-		},
+    'gh-pages': {
+      options: {
+        base: 'dist'
+      },
+      src: '**/*'
+    },
 
-		multiresize: {
-			target: {
-      	src: 'src/misc/logo-cookie.png',
-      	dest:[
-					'src/CookiebarGone.safariextension/Icon-32.png',
-					'src/CookiebarGone.safariextension/Icon-48.png',
-					'src/CookiebarGone.safariextension/Icon-64.png',
-					'src/CookiebarGone.safariextension/Icon-96.png',
-					'src/CookiebarGone.safariextension/Icon-128.png',
+    multiresize: {
+      Safari: {
+        src: 'src/misc/logo-cookie.png',
+        dest: [
+          'src/CookiebarGone.safariextension/Icon.png',
+          'src/CookiebarGone.safariextension/Icon-32.png',
+          'src/CookiebarGone.safariextension/Icon-48.png',
+          'src/CookiebarGone.safariextension/Icon-64.png',
+          'src/CookiebarGone.safariextension/Icon-96.png',
+          'src/CookiebarGone.safariextension/Icon-128.png',
 
-				],
-	      destSizes: ['32x32','48x48','64x64','96x96','128x128']
-    	}
-  	},
+        ],
+        destSizes: ['32x32', '32x32', '48x48', '64x64', '96x96', '128x128']
+      },
+      SafariStore: {
+        src: 'src/misc/logo-cookie.png',
+        dest: ['src/misc/icon-256.png'],
+        destSizes: ['256x256']
+      }
+    },
+    jshint: {
+      all: ['Gruntfile.js', 'package.json', 'src/scripts/*.js']
+    },
 
-	});
+  });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-file-exists');
-	grunt.loadNpmTasks('grunt-gh-pages');
-	grunt.loadNpmTasks('grunt-multiresize');
+  grunt.loadNpmTasks('grunt-gh-pages');
+  grunt.loadNpmTasks('grunt-multiresize');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
-	grunt.registerTask('build',['uglify']);
-  grunt.registerTask('deploy', ['copy','fileExists','gh-pages']);
+  grunt.registerTask('build', ['uglify']);
+  grunt.registerTask('deploy', ['copy', 'fileExists', 'gh-pages']);
 
 };
--- src/scripts/main_logic.js
+++ src/scripts/main_logic.js
@@ -4,33 +4,32 @@ var cookie_timeout = 150;
 var cookie_attempt = 1;
 
 // Try to click a button by a certain querySelector
-function cookie_gone_try_query(q){
+function cookie_gone_try_query(q) {
   console.log(q);
   var htmlNode = document.querySelector(q);
-  if(htmlNode)
-  {
+  if (htmlNode) {
     htmlNode.click();
     return true;
   }
 }
 
 // Try to click a button by a certain id
-function cookie_gone_try_id(id){
+function cookie_gone_try_id(id) {
   console.log(id);
   var htmlnode = document.getElementById(id);
-  if(htmlnode){
-    console.log('Found ID: '+id);
+  if (htmlnode) {
+    console.log('Found ID: ' + id);
     htmlnode.click();
     return true;
   }
 }
 
 //Try to click a button by classname
-function cookie_gone_try_class(cls){
+function cookie_gone_try_class(cls) {
   console.log(cls);
   var htmlnode = document.getElementsByClassName(cls);
-  if(htmlnode.length){
-    console.log('Found Class: '+cls);
+  if (htmlnode.length) {
+    console.log('Found Class: ' + cls);
     htmlnode[0].click();
     return true;
   }
@@ -39,37 +38,37 @@ function cookie_gone_try_class(cls){
 
 // This is the main function of the extension.
 // It is called on a timeout because some sites delay to cookiemelding.
-function remove_all_cookie_bars(){
+function remove_all_cookie_bars() {
 
   //Check all IDS and click them.
   console.log('Trying cookie ids');
-  for(index in cookie_btn_ids){
-    if(cookie_gone_try_id(cookie_btn_ids[index]))
+  for (var idIndex in cookie_btn_ids) {
+    if (cookie_gone_try_id(cookie_btn_ids[idIndex]))
       return true;
   }
 
   //Check all the classes and click them.
   console.log('Trying cookie classes');
-  for(index in cookie_btn_classes){
-    if(cookie_gone_try_class(cookie_btn_classes[index]))
+  for (var clsIndex in cookie_btn_classes) {
+    if (cookie_gone_try_class(cookie_btn_classes[clsIndex]))
       return true;
   }
 
   //Check all the queries (most expensive)
   console.log('Trying cookie queries');
   //log(cookie_btn_queries);
-  for(index in cookie_btn_queries){
-    if(cookie_gone_try_query(cookie_btn_queries[index]))
+  for (var queryIndex in cookie_btn_queries) {
+    if (cookie_gone_try_query(cookie_btn_queries[queryIndex]))
       return true;
   }
 
   // Retry 4 times, make the delay longer on every call.
-  if(cookie_attempt < 5){
-    setTimeout(remove_all_cookie_bars,cookie_timeout*cookie_attempt);
+  if (cookie_attempt < 5) {
+    setTimeout(remove_all_cookie_bars, cookie_timeout * cookie_attempt);
   }
   cookie_attempt++;
   return false;
 }
 
 // Execute the main funtion after 50ms
-setTimeout(remove_all_cookie_bars,50);
+setTimeout(remove_all_cookie_bars, 50);
